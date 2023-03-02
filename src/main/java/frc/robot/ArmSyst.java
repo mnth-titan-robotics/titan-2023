1933970a7755ccd613877d57a4b651a309dbd059
Ethan-S <emscranton@icloud.com>
2023-03-02 13:27:27 -0600

    Arm motor speed changed to .5

--- src/main/java/frc/robot/ArmSyst.java
+++ src/main/java/frc/robot/ArmSyst.java
@@ -15,8 +15,8 @@ public class ArmSyst {
         }
 
         public void update(double armCoStickL) {
-        Motor_ARM_1.set(armCoStickL + .4);
-        Motor_ARM_2.set(armCoStickL * -1 + .4);
+        Motor_ARM_1.set(armCoStickL + .5);
+        Motor_ARM_2.set(armCoStickL * -1 + .5);
         }
 
     }
